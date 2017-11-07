76d55fd0fa2c575cddf29aed5e84a88c2020df4a
Roman <Duncan1roma@gmail.com>
2017-11-07 18:04:11 +0500

    small fix

--- controllers/ninjaController.php
+++ controllers/ninjaController.php
@@ -32,7 +32,7 @@ class ninjaController extends Controller {
 		$_PUT=json_decode(file_get_contents('php://input'),true);
 		
 		if((isset($_PUT['id'])) && (isset($_PUT['name'])) && (isset($_PUT['image'])) && (isset($_PUT['power'])) && (isset($_PUT['speed']))) {
-			$dataToUpdate=array('id'=>$_POST['id'],'name'=>$_POST['name'],'image'=>$_POST['image'], 'power'=>$_POST['power'], 'speed'=>$_POST['speed']);
+			$dataToUpdate=array('id'=>$_PUT['id'],'name'=>$_PUT['name'],'image'=>$_PUT['image'], 'power'=>$_PUT['power'], 'speed'=>$_PUT['speed']);
 			$updatedItem=$this->model->save($data['id'], $dataToUpdate);
 			$this->setResponce($updatedItem);
 			}
